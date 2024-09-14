f14aacd15af4ddd0e7eec2d9766b467acbcd4fc4
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2024-09-14 14:59:06 +0900

    Update periodic_trace.sh

--- devel_opment/debug/monitoring_by_user/periodic_trace.sh
+++ devel_opment/debug/monitoring_by_user/periodic_trace.sh
@@ -10,7 +10,7 @@ do
   date
   date > $this_path.log
   echo "save date info to $this_path.log"
-  vcgencmd measure_temp > $this_path.temp.cpu.log > 2>&1 
+  vcgencmd measure_temp > $this_path.temp.cpu.log 2>&1 
   sleep 30
   clear
 done
