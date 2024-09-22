e28707ed93a8c8b460aad2570ddce62eb70f0ca1
TinyOS Lover (3PO_Kang) <gadin.kang@gmail.com>
2024-09-22 23:06:29 +0900

    Update return_from_python.sh

--- devel_opment/shell_script/return_from_python.sh
+++ devel_opment/shell_script/return_from_python.sh
@@ -13,3 +13,32 @@ if [ $exit_status -eq 0 ]; then
 else
     echo "Python script failed with status $exit_status"
 fi
+
+
+
+# 센서 데이터를 계산하거나 받아오기
+sensor_data = 123.456
+
+# 데이터를 표준 출력에 출력
+print(sensor_data)
+
+
+
+sensor_data = 123.456
+
+# 파일에 데이터 쓰기
+with open("sensor_data.txt", "w") as file:
+    file.write(str(sensor_data))
+
+
+#!/bin/bash
+
+# Python 스크립트 실행
+python write_data.py
+
+# 파일에서 데이터 읽기
+read sensor_data < sensor_data.txt
+
+# 데이터 출력
+echo "Received sensor data: $sensor_data"
+
