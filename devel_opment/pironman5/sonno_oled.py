65e12289d56121df5e99d03326b6c8dec6cef1ad
joenghoonkang <gadin.kang@gmail.com>
2024-10-26 11:55:49 +0900

    touch crontab path

--- devel_opment/pironman5/sonno_oled.py
+++ devel_opment/pironman5/sonno_oled.py
@@ -1,5 +1,9 @@
 # Author : https://github.com/jeonghoonkang 
+
 from pm_auto.pm_auto import PMAuto
+
+### This file run by sudo crontab
+### @reboot python3 /home/tinyos/devel_opment/selfcloud/devel_opment/pironman5/sonno_oled.py
 
 config = {
   #"peripherals": ['oled','ws2812','gpio_fan','pwm_fan',],
@@ -13,7 +17,7 @@ pm = PMAuto(temp1)
 ### print (pm)
 
 ### decide what to run
-### 1 shot
+### one shot
 pm.onshot()
 
 ### periodic every 1 sec.
