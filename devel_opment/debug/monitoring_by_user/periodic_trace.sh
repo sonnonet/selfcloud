b41650ef02ab9d949e759ccacc1038a98096c096
joenghoonkang <gadin.kang@gmail.com>
2024-10-26 11:04:59 +0900

    ourcode

--- devel_opment/debug/monitoring_by_user/periodic_trace.sh
+++ devel_opment/debug/monitoring_by_user/periodic_trace.sh
@@ -11,8 +11,8 @@ do
   date > $this_path.log
   echo "save date info to $this_path.log"
   sudo vcgencmd measure_temp > $this_path.temp.cpu.log 2>&1 
-  echo "fan speed :"
-  sudo cat /sys/class/thermal/cooling_device0/cur_state
-  sleep 30
+  echo "fan speed :" >> $this_path.temp.cpu.log 2>&1 
+  sudo cat /sys/class/thermal/cooling_device0/cur_state >> $this_path.temp.cpu.log 2>&1
+  sleep 60
   clear
 done
